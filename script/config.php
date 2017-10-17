bb54762174c976ff77414c1eee9d3abfee19d05d
Morgan Boreatti <m.boreatti@gmail.com>
2017-10-17 16:06:55 +0200

    Delete config.php

--- script/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-// On declare un tableau de configuration vide 
-$config = [];
-$config['dbname'] = 'eldinn3WA';
-$config['dbhost'] = 'localhost';
-$config['dbuser'] = 'root';
-$config['dbpassword'] = 'yolo';
-
